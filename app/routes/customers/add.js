2a275dfbd05e267d931f484ebd71dbd1d56bd2fb
Captaiin <atharvabelapurkar007@gmail.com>
2023-04-26 21:14:47 +0530

    Added some changes

--- app/routes/customers/add.js
+++ app/routes/customers/add.js
@@ -1,3 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default class CustomersAddRoute extends Route {}
+export default class CustomersAddRoute extends Route {
+  model() {
+    return this.modelFor('customers');
+  }
+}
